66938b7d16b25ba4f5bd56677fafea33d4e6a4fb
Your's YK <yashraj.enter1@gmail.com>
2025-09-21 10:12:55 +0000

    use this api key for signup and login
    AIzaSyDw9M3mco4SwCwPfcZssoqaIFJgg

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,14 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // IMPORTANT: This is a public configuration and is safe to expose.
 // Security is handled by Firebase Security Rules and App Check.
-const firebaseConfig = {"apiKey":"FIREBASE_API_KEY","authDomain":"FIREBASE_AUTH_DOMAIN","projectId":"FIREBASE_PROJECT_ID","storageBucket":"FIREBASE_STORAGE_BUCKET","messagingSenderId":"FIREBASE_MESSAGING_SENDER_ID","appId":"FIREBASE_APP_ID"};
+const firebaseConfig = {
+  apiKey: 'AIzaSyDw9M3mco4SwCwPfcZssoqaIFJggI18PGQ',
+  authDomain: 'legali-39226.firebaseapp.com',
+  projectId: 'legali-39226',
+  storageBucket: 'legali-39226.appspot.com',
+  messagingSenderId: '472283993425',
+  appId: '1:472283993425:web:a96be3362a3f7491d9d936',
+};
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
